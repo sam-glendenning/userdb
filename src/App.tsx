183a1d79985c52dc021318c54fd4b609c4da8e6b
Sam Glendenning <samglendenning@duck.com>
2023-07-04 14:31:52 +0100

    Adding tabular layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import "./App.css";
 import RemoveUserbox from "./removeUserBox";
 import AddUserBox from "./addUserBox";
 import { useUserList } from "./requests";
@@ -26,12 +25,26 @@ const App = (): React.ReactElement => {
         <br />
         {usersLoading && <p>Loading users...</p>}
         {usersError && <p>Error fetching users!</p>}
-        {users &&
-          users.data.map((user, i) => (
-            <p key={i}>
-              {user.id}. {user.email}
-            </p>
-          ))}
+        {users && (
+          <table>
+            <tbody>
+              <tr>
+                <th>Id</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+              </tr>
+              {users.data.map((user, i) => (
+                <tr key={i}>
+                  <td>{user.id}</td>
+                  <td>{user.first_name}</td>
+                  <td>{user.last_name}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
       <AddUserBox />
       {!!users && users.data.length >= 1 && (
